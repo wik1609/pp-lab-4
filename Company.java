286b5c8ebcc8bb9d1ad677baab3904164aff4d06
Wiktor Krawczak <wiko3034gmail.com>
2024-04-14 12:53:45 +0200

    zmienione na metode toString

--- Company.java
+++ Company.java
@@ -3,7 +3,7 @@ import employees.Manager;
 import employees.Worker;
 public class Company {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[7]; //tablica zmieniona na 7 pracownikow
+        Employee[] employees = new Employee[12]; //tablica zmieniona na 7 pracownikow
         employees[0] = new Manager("Jan Tura" , 4000); //zmodyfikowane jako dodanie managera dla indeksu 0
         employees[1] = new Worker("Konrad Duda", 5000, "Technik"); //zmodyfikowane jako dodanie Worker
         employees[2] = new Employee("Kuba Dym", 6000);
@@ -11,6 +11,11 @@ public class Company {
         employees[4] = new Worker("Patryk Stary", 7000, "Inżynier"); //zmodyfikowane jako dodanie Worker
         employees[5] = new Employee("Piotrek Bąk", 3500); //nowy pracownik
         employees[6] = new Manager("Michał Nowark", 5600); //nowy manager
+        employees[7] = new Employee("Andrzej Wróbel", 4100); //nowy pracownik
+        employees[8] = new Employee("Jakub Krawczyk", 3900); //nowy pracownik
+        employees[9] = new Employee("Piotr Kula", 4200); //nowy pracownik
+        employees[10] = new Employee("Damian Bomba", 3600); //nowy pracownik
+        employees[11] = new Employee("Olaf Gołota", 3800); //nowy pracownik
 
         employees[3].setSalary(5000); //zmiana wynagrodzenia dla pracownika 3 a potem dodanie +500
         
@@ -24,7 +29,7 @@ public class Company {
         for (Employee employee : employees) {
             if (employee instanceof Manager) {
                 Manager manager = (Manager) employee;
-                manager.setNumberOfSubordinates(5); //5 to liczba pracownikow
+                manager.setNumberOfSubordinates(7); //7 to liczba pracownikow
                 manager.setSalary(7500); //zmiana wynagrodzenia dla managerów na 7500
             }
         }
@@ -33,7 +38,7 @@ public class Company {
         System.out.println("Nowe informacje o pracownikach: "); //wyswiietlanie nowych informacji o pracownikach
         for (Employee employee : employees) {
             if (employee != null) {
-                System.out.println(employee);
+                System.out.println(employee.toString());
             }
         }
     }
